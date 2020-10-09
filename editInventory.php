11be84673a04ef76f3b9aa607ea1f23d77a21015
babychamp <mrchandimawijebadnara@gmail.com>
2020-10-09 18:43:49 +0800

    changes to editInvt

--- editInventory.php
+++ editInventory.php
@@ -1,1 +1,14 @@
-edit inverntory
+<?php
+$servername = "localhost";
+$DBusername = "root";
+$DBpassword = "";
+$DBname = "fcms";
+
+$conn = mysqli_connect($servername, $DBusername, $DBpassword, $DBname);
+
+// Check connection
+if (!$conn){
+  die("Connection failed:" .mysqli_connect_error());
+}
+
+?>
