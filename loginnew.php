2a8f699d21c123bb1363e90f374e358c1c9c6a65
babychamp <mrchandimawijebadnara@gmail.com>
2020-10-21 10:29:32 +0800

    working login module with login status (type of user)

--- loginnew.php
+++ loginnew.php
@@ -1,11 +1,9 @@
 <?php
   // start session
-  session_start();
+  //session_start();
+    
   require "Include/header.php";
  ?>
-
-
-
 
 <?php
 
@@ -25,32 +23,29 @@
 		return $conn;
 		}
 	$conn = connect();
-    
 
-
-
-		
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	test_input($_POST["username"],$_POST["pwd"]);
+	test_input($_POST["username"],$_POST["password"]);
 }
 
 function test_input($username,$password) {
 	$match = false;
-	$id = 0;
+	$customerID = 0;
+    $type = 0;
 	$username = trim($username);
 	$password = trim($password);
 	$conn = connect();
-	$sql = "SELECT * FROM users";
+	$sql = "SELECT * FROM userdb";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			if($row["username"]==$username || $row["password"]==$password){
+			if($row["username"]==$username && $row["password"]==$password){
 				$match = true;
-				$id = $row["id"];
-				$status = $row["status"];
+				$id = $row["customerID"];
+				$type = $row["type"];
 			}
 		}
 	} else {
@@ -60,14 +55,13 @@ function test_input($username,$password) {
 		session_start();
 		$_SESSION["loggedin"] = true;
 		$_SESSION["username"] = $_POST["username"];
-		$_SESSION["loginid"] = $id;
-		$_SESSION["loginstatus"] = $status;
-		if($status == "admin"){
-			header("refresh: 2; url=loginnew.php");
+		$_SESSION["loginid"] = $customerID;
+		$_SESSION["loginstatus"] = $type;
+		if($type == "1" || $type == "2" || $type == "3"){
+			header("refresh: 0; url=index.php");
 		}else{
-			header("refresh: 2; url=index.php");
+			header("refresh: 0; url=loginnew.php");
 		}
-		
 	}
 }?>
 
@@ -76,15 +70,13 @@ function test_input($username,$password) {
     <form id="login" method="POST">
         <div class="form-header">
             <h3>Login</h3>
-            <!--<p>Please fill in the follwoing details</p> -->
         </div>
         <div class="form-line"></div>
         <div class="inputs">
             <input type="text" name="username" placeholder="Username" required />
-            <input type="password" name="pwd" placeholder="Password" required />
+            <input type="password" name="password" placeholder="Password" required />
 
-            <button type="submit" name="login-btn">LOGIN</button>
-            <!-- <a id="register-submit">SIGN UP</a> -->
+            <button type="submit" name="login-btn">LOG IN</button>
         </div>
     </form>
 </div>
