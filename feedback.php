f2ccfe2d728dd50f4c30fbb458ae8368c87375ae
newbie2345 <62095948+newbie2345@users.noreply.github.com>
2020-11-05 21:18:27 +0800

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -1,1 +1,43 @@
-test
+<?php
+	// start the session()
+	session_start();
+	if (!empty($_SESSION['uid'])){
+		$custId = $_SESSION['uid'];
+	}else{
+		echo '<script type="text/javascript">
+					alert("Invalid Session!");
+					window.location = "index.php";
+				</script>';
+		exit();
+	}
+	
+	require "Include/header.php";
+	
+	include("Include/dtb.php");
+
+	if (isset ($_POST['fbDesc'])){
+			$get_email = mysqli_escape_string($conn, $_POST['email']);
+			$get_feedback = mysqli_escape_string($conn, $_POST['fbDesc']);
+			
+			$sql_ins = $conn -> prepare ("INSERT INTO feedbackdb (customerID,feedbackDesc, customerEmail) VALUES(?, ?, ?)");
+			$sql_ins->bind_param("iss", $custId, $get_feedback, $get_email);
+            $sql_ins->execute();
+			mysqli_close($conn);
+	}else{
+		echo 'error';
+	}
+?>
+
+<div id="testing">
+<h1>Give Us Your Feedback</h1>
+<h2>What Did You Think?</h2>
+<form name="feedback" action="feedback.php" method="POST">
+<label for="email">Your Email: </label><br>
+<input type="text" id="email" name="email"><br>
+<br>
+<label for="fbDesc">Feedback to us: </label><br>
+<textarea id="fbDesc" name="fbDesc" rows="4" cols="150"></textarea><br>
+<br>
+<input type="submit" value="Send">
+</form>
+</div>
