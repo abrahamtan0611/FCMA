9119b759669b5830f497acca49ae9dd4ead21387
abrahamtan0611 <62094075+abrahamtan0611@users.noreply.github.com>
2020-11-06 20:12:27 +0800

    Add files via upload

--- Include/reset-request.php
+++ Include/reset-request.php
@@ -1,10 +1,10 @@
 <?php
+require('../PHPMailer/PHPMailerAutoload.php');
 
-if(isset($_POST["reset-request-submit"])){
-    
+if(isset($_POST["reset-request-submit"])){  
     $selector = bin2hex(random_bytes(8));
     $token = random_bytes(32);
-    $url = "create-new-pw.php?selector=" .$selector."&validator=" . bin2hex($token);
+    $url = "localhost/FCMS-msater/create-new-pw.php?selector=" .$selector."&validator=" . bin2hex($token);
     
     $expires = date("U") + 1800;
     
@@ -53,12 +53,23 @@ if(isset($_POST["reset-request-submit"])){
     $headers .= "Content-type: text/html\r\n";
     
     mail($to, $subject, $message, $headers);
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls';
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 587;
+	$mail->isHTML();
+	$mail->Username = 'foodedgegourmetcatering@gmail.com';
+	$mail->Password = 'foodedgeswin';
+	$mail->setFrom('no-reply@fcms.com', 'no-reply@fcms.com');
+	$mail->Subject = $subject;
+	$mail->Body = $message;
+	$mail->AddAddress($to);
+
+	$mail->Send();
     header("Location: ../resetpw.php?reset=success");
-        
 }else{
-    header("Location:../index.php");
-    
-}
-
-
+	header("Location:../index.php");
+}	
 ?>
