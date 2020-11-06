2e588ed3b286e618ca4878ff65b181ea1efa03fb
101214093 <62094835+101214093@users.noreply.github.com>
2020-11-06 20:30:07 +0800

    Add files via upload

--- ajax.php
+++ ajax.php
@@ -33,6 +33,24 @@
 		echo "$title has been successfully updated";
 	}
 	
+	//dont touch
+	if(isset($_POST['orderid']))
+	{
+		include("Include/dtb.php");
+		$title = $_POST['title'];
+		$desc = $_POST['desc'];
+		$price = $_POST['price'];
+		$id = $_POST['menuid'];
+		
+		$sql = "UPDATE inventorydb SET name = '$title' , description = '$desc', price = $price WHERE menuID = $id";
+		
+		mysqli_query($conn, $sql);
+		
+		mysqli_close($conn);
+		
+		echo "$title has been successfully updated";
+	}
+	
 	if (isset($_POST['orderid'])) {
 		include("Include/dtb.php");
 		$id = $_POST['orderid'];
@@ -40,19 +58,21 @@
 		$sqldata = mysqli_query($conn, $sql)or die(mysqli_error($conn));
 		$row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC);
 		$name = $row['orderID'];
-
+		
 		$sql2 = "Delete From orderdb Where orderID =$id";
 		mysqli_query($conn, $sql2);
 
 		mysqli_close($conn);
 
-		echo "$name has been successfully deleted";
+		echo "Order has been successfully deleted";
 	}
 	
 	if(isset($_POST['menuID'])){
 		session_start();
 		$_SESSION['indexMenuID'] = $_POST['menuID'];
 	}
+	
+	
 ?>
 
 
--- javascript.js
+++ javascript.js
@@ -51,6 +51,52 @@ function deleteData(e){
 	});
 }
 
+//dont touch
+function editSortOrder(e){
+	var modal = document.getElementById("myModal");
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName("close")[0];
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+		modal.style.display = "none";
+	  }
+	}
+}
+
+function editOrder(e){
+	// Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+		modal.style.display = "none";
+	  }
+	}
+}
+
 function edit_onclick(e)
 {
 	var edit_btn = document.getElementById(e);
