754305da5966b35fd09895dc94eca0812e7a1117
abrahamtan0611 <62094075+abrahamtan0611@users.noreply.github.com>
2020-11-06 20:26:53 +0800

    Add files via upload

--- Include/header.php
+++ Include/header.php
@@ -20,6 +20,7 @@
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<title>Food Edge</title>
 	<script>
 		$(function(){
