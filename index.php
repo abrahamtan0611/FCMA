fffd20c21be148eaf74e891388e81659ed27a374
abrahamtan0611 <62094075+abrahamtan0611@users.noreply.github.com>
2020-11-09 21:44:42 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,81 +1,70 @@
 <?php
 	// start the session()
 	session_start();
-	require "Include/header.php";
+	require_once "Include/header.php";
 ?>
-<!--
-<script>
-	
-	function menuFunction(e){
-		alert("asdasd");
-		//var modal2 = document.getElementById("myModal");
-		//modal2.style.display = "block";
-		var menuID = e.substr(11);
-		
-		$.ajax({
-		type: "POST",
-		url:'ajax.php',
-		data:{menuID:menuID},
-		success:function(html){
-			//window.location.replace("order_alt.php");
-			alert(menuID);
-		}
-		});
 
-	}
-	
-</script>
--->
-	<?php
-		include("Include/dtb.php");
-		$sqlget = "SELECT * FROM inventorydb LIMIT 8";
-		$sqldata = mysqli_query($conn, $sqlget) or die("error getting data");
-		
-	?>
-		<div id="title" class="row">
-			<div class="col-md-12">
-				<h1>Hot Sell!!!
-				<?php
-					if(isset($_SESSION['type'])){
-						if ($_SESSION['type'] == 2){
-							echo '<button type="button" class="btn btn-success btn-lg" id="myBtn" onclick="redirect()">Edit</button>';
-						}	
-					}
-				?>
-				
-				
-				
-				</h1>
+<?php
+	include_once("Include/dtb.php");
+	$sqlget = "SELECT * FROM productdb LIMIT 3";
+	$sqldata = mysqli_query($conn, $sqlget) or die("error getting data");	
+?>
+<!-- BANNER -->
+<div class="header">
+	<div class="container">
+		<div class="row">
+			<div class="col-2">
+				<h1>FoodEdge Gourmet <br>Catering Service</h1>
+				<p>FoodEdge Gourmet Catering is the perfect caterer for all your party, wedding, holiday and all kinds of events. We can satisfy all you needs with our experience in catering. We do our best to serve you right!</p>
+				<a href="product.php" class="btn">Order Now &#8594;</a>
 			</div>
-		</div>
-		<div id="article" class="row">
-			<div id="menu" class="col-md-12">
-				<?php
-					while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
-				?>	
-					<div class="food_col" id="food_col_id<?php echo $row['menuID']?> "onclick="menuFunction(this.id)">
-						<img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" width="308px" height="200px"/> 
-						<p class="food_title"><?php echo $row["name"]?> <span class="rating"><span class="fa fa-star checked"></span>4.4/5(64)</span></p>
-						<p class="food_desc"><?php echo $row['description'] ?></p>
-						<p class="food_price">RM<?php echo number_format($row['price'],2) ?></p>
-					</div>
-				<?php
-					}
-				?>
-			</div>
-		
-		</div>
-
-		
-		<div id="footer" class="row">
-			<div class="col-md-12">
-				<!-- put your footer here -->
-				<p>Footer</p>
+			<div class="col-2">
+				<img src="Images/banner.png">
 			</div>
 		</div>
 	</div>
+</div>
+		
+<!-- FEATURE PRODUCT -->
+<div class="feature">
+	<div class="small-container">
+		<h2 class="title">Featured Products</h2>
+		<div class="row">
+			<?php
+				while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
+			?>
+			<div class="col-3" id="food_col_id<?php echo $row['productID']?>"onclick="menuFunction(this.id)">
+				<img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image'])?>" width="100px" height="350px"/>
+				<h4><?php echo $row['name'] ?></p>
+			</div>
+			<?php
+				}
+			?>
+		</div>
+	</div>
+</div>
 
-</body>
-</html>
-
+<!-- FEEDBACKS -->
+<div class="feedbacks">
+	<div class="container">
+		<h2 class="title"> Customer Feedbacks</h2>
+		<div class="row">
+			<div class="col-3">
+				<p>This is feedback item.hajsdhgajkhdajkdh ajkdhasjkdhaskjdhaskjahdkjhdasgd hasgdhasgdhjasgdahjgahjgah jdghjad gahjdgahjagh jsdgahjdgajhdgajhdgaduhag</p>
+				<h3>Name</h3>
+			</div>
+			<div class="col-3">
+				<p>This is feedback item.hajsdhgajkhdaj kdhajkdh asjkdhaskjdha skjahdkjh</p>
+				<h3>Name</h3>
+			</div>
+			<div class="col-3">
+				<p>This is feedback item.hajsdhga jkhdajkd hajkdhas jkdha skjdhaskj ahdkjh</p>
+				<h3>Name</h3>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+ include_once "Include/footer.php"; 
+ ?>
 
