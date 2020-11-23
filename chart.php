86f17b2a94e6a762b3651e959753754f0c7b2eda
babychamp <mrchandimawijebadnara@gmail.com>
2020-11-23 19:21:36 +0800

    Chart: Added working year filter with drop-down list of years populating from distinct years in orderDb

--- chart.php
+++ chart.php
@@ -1,80 +1,100 @@
+<!doctype html public "-//w3c//dtd html 3.2//en">
+<html>
+
+<head>
+    <title>Bar Chart of Sales/profit</title>
+    <style>
+
+
+    </style>
+
+    <!--    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> 
+-->
+</head>
+
+
+
 <?Php
-session_start();
 require 'Include/dtb.php';
 require 'Include/header.php';
     
-  
- //$year = 2020;
-//(paymentStatus != 'pending' && YEAR(deliveryDate) = $year)
-//$year = datalistvalue
-    if($stmt = $conn->query("SELECT DATE_FORMAT(deliveryDate, '%M %Y'), (totalAmount-totalOriPrice) AS Profit, totalAmount AS Sales FROM orderdb WHERE (paymentStatus != 'pending') GROUP BY YEAR(deliveryDate), MONTHNAME(deliveryDate) ORDER BY deliveryDate")){
-  
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $year = $_POST['year'];
+   // if (empty($year)) {
+  //  $year = date("Y");
+  //  } else {
+ //   echo $year;
+  //  }
+    } else{
+        $year = date("Y");
+    }
+ 
+    if($stmt = $conn->query("SELECT DATE_FORMAT(deliveryDate, '%M %Y'), (totalAmount-totalOriPrice) AS Profit, totalAmount AS Sales FROM orderdb WHERE (paymentStatus != 'pending' && YEAR(deliveryDate)= $year) GROUP BY YEAR(deliveryDate), MONTHNAME(deliveryDate) ORDER BY deliveryDate")) 
+    {  
+        
+        
 
-	$php_data_array = Array(); // create PHP array
+        
+}else{
+echo $conn->error;
+}
+    $php_data_array = Array(); 
+while ($row = $stmt->fetch_row()) {
+   $php_data_array[] = $row; 
+   }
 
-		while ($row = $stmt->fetch_row()) {
-		   $php_data_array[] = $row; // Adding to array
-		}
-
-	}else{
-		echo $conn->error;
-	}
-	echo'
-	<div id="content">
-	<div>
-	<input type ="date" name = "from_date" id = "from_date"/>
-	&nbsp;
-	<input type ="date" name = "to_date" id = "to_date"/>
-
-	</div>
-	<br>
-	';
-    
 
 ?>
-<select name="year" class="form-control" id="year">
-    <option value="">Select Year</option>
-    <option value="2016">2016</option>
-    <option value="2017">2017</option>
-    <option value="2018">2018</option>
-    <option value="2019">2019</option>
-    <option value="2020">2020</option>
+
+
+<h2 style="text-align:center">Food Edge: Month-wise Profit vs Sales information for year: &nbsp;<?php echo $year; ?></h2>
+<h4> Use the below list to filter by the year: </h4>
+
+<!--
+<form method="post" action="<?php //echo $_SERVER['PHP_SELF'];?>">
+    <input list="year" name="year">
+    <datalist id="year">
+        <option value="">Select Year</option>
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+        <option value="2018">2018</option>
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+    </datalist>
+    <input type="submit">
+</form>
+
+
+-->
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
     <?php
-	foreach($stmt as $row)
-	{
-		echo '<option value="'.$row[$year].'">'.$row[$year].'</option>';
-	}
-	?>
-</select>
-<button type="submit" name="submit" id="submit"onClick="myFunction();">Filter</button>
-<br>
+   $query = mysqli_query($conn, "SELECT DISTINCT YEAR(deliveryDate) AS year from orderDb");
+   echo "<input type='search' name = 'year' list = 'year'/>";
+    echo "<datalist id = 'year'>";
+    while($row=mysqli_fetch_array($query))
+{
+    echo "<option>$row[year] </option>";
+}
+    echo"</datalist>";
+?>
+    <input type="submit">
+</form>
+
 <?php
     
-    //SELECT GETDATE() 'Today', FORMAT(deliveryDate,'MMMM') 'Month Name'
-    
-    //SELECT RIGHT(CONVERT(VARCHAR(8), deliveryDate, 3), 5) AS [MM/YY]
-    
-    // FORMAT(deliveryDate,'MMyy')
-    
-    // mysql> SELECT DATE_FORMAT('2009-10-04 22:23:00', '%W %M %Y');
-    //outputs  -> 'Sunday October 2009'
-
-
-//print_r( $php_data_array);
-// You can display the json_encode output here. 
-//echo json_encode($php_data_array); 
-
-// Transfor PHP array to JavaScript two dimensional array 
 echo "<script>
-        var my_2d = ".json_encode($php_data_array)."
-		</script>";
+        var insights = ".json_encode($php_data_array)."
+</script>";
 ?>
 
-
-<div id="chart_div">
-
-</div>
+<div id="control1"></div>
+<div id="chart_div"></div>
 <br><br>
+
+
 <script type="text/javascript">
     // Load the Visualization API and the corechart package.
     google.charts.load('current', {
@@ -82,15 +102,20 @@ echo "<script>
     });
     google.charts.setOnLoadCallback(drawChart);
 
+
+
     function drawChart() {
+
+
+
 
         // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Month');
         data.addColumn('number', 'Profit');
         data.addColumn('number', 'Sales');
-        for (i = 0; i < my_2d.length; i++)
-            data.addRow([my_2d[i][0], parseInt(my_2d[i][1]), parseInt(my_2d[i][2])]);
+        for (i = 0; i < insights.length; i++)
+            data.addRow([insights[i][0], parseInt(insights[i][1]), parseInt(insights[i][2])]);
         var options = {
             title: 'Food Edge Sales Profit',
             hAxis: {
@@ -104,28 +129,29 @@ echo "<script>
                 minValue: 0
             }
         };
-
+        /*
+                var yearPicker = new google.visualization.ControlWrapper({
+                    'controlType': 'CategoryFilter',
+                    'containerId': 'control1',
+                    'options': {
+                        'filterColumnLabel': 'Month',
+                        'ui': {
+                            'labelStacking': 'vertical',
+                            'allowTyping': false,
+                            'allowMultiple': false,
+                            'allowNone': false
+                        }
+                    },
+                    'state': {
+                        selectedValues: ['October']
+                    }
+                });
+        */
         var chart = new google.charts.Bar(document.getElementById('chart_div'));
         chart.draw(data, options);
     }
-	
-</script>
-<script>
-    $(document).ready(function() {
-
-        $('#year').change(function() {
-            var year = $(this).val();
-            if (year != '') {
-                load_monthwise_data(year, 'Month Wise Profit Data For');
-            }
-        });
-
-    });
 
 </script>
-</div>
-</div>
-<?php
-include_once "Include/footer.php"; 
-?>
+</body>
 
+</html>
